231c9c6eb0b53f712a6e67e140de665bfd0d0a40
Kiu0911 <115052437+Kiu0911@users.noreply.github.com>
2022-10-26 09:00:08 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+let x_value = 0
+let y_value = 0
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(x_value, y_value)
+            basic.pause(200)
+            x_value += 1
+        }
+        y_value += 1
+        y_value = 0
+    }
+})
+basic.forever(function () {
+	
+})
